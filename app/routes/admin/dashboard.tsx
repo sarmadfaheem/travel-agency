e9b7669adaf3abbde41914c3d4d8c782203de1d0
Sarmad Faheem <muhammadsarmad302@gmail.com>
2026-04-01 18:00:43 +0500

    feat: make the stats card

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,49 @@
-import { Header } from "components";
+import { Header, StatsCard, TripCard } from "components";
 
 const Dashboard = () => {
   const user = {
     name: "Sarmad",
   };
+  const dashboardStats = {
+    totalUsers: 12450,
+    usersJoined: { currentMonth: 218, lastMonth: 1176 },
+    totalTrips: 3210,
+    tripsCreated: { currentMonth: 150, lastMonth: 250 },
+    userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
+  };
+
+  const { totalTrips, totalUsers, tripsCreated, userRole, usersJoined } =
+    dashboardStats;
+
   return (
     <main className="dashboard wrapper">
       <Header
         title={`Welcome ${user?.name ?? "Guest"} 👋`}
         description="Track activity, trends and popular destination in real time"
       />
-      DashBoard Page Content
+
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full"></div>
+      </section>
+      <StatsCard
+        headerTitle="Total Users"
+        total={totalUsers}
+        currentMonthCount={usersJoined.currentMonth}
+        lastMonthCount={usersJoined.lastMonth}
+      />
+      <StatsCard
+        headerTitle="Total Trips"
+        total={totalTrips}
+        currentMonthCount={tripsCreated.currentMonth}
+        lastMonthCount={tripsCreated.lastMonth}
+      />
+      <StatsCard
+        headerTitle="Active Users Today"
+        total={userRole.total}
+        currentMonthCount={userRole.currentMonth}
+        lastMonthCount={userRole.lastMonth}
+      />
+      <TripCard />
     </main>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as NavItems } from "./nav-items";
 export { default as MobileSidebar } from "./mobile-sidebar";
 export { default as Header } from "./header";
+export { default as TripCard } from "./trip-card";
+export { default as StatsCard } from "./stats-card";
