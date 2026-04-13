4f9cd3150a828229d5fb05bde77d9a40e2635202
Sarmad Faheem <muhammadsarmad302@gmail.com>
2026-04-13 12:40:14 +0500

    feat: add the trip card and their dummy data

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,20 +1,10 @@
 import { Header, StatsCard, TripCard } from "components";
+import { dashboardStats, user, allTrips } from "~/constants";
+
+const { totalTrips, totalUsers, tripsCreated, userRole, usersJoined } =
+  dashboardStats;
 
 const Dashboard = () => {
-  const user = {
-    name: "Sarmad",
-  };
-  const dashboardStats = {
-    totalUsers: 12450,
-    usersJoined: { currentMonth: 218, lastMonth: 1176 },
-    totalTrips: 3210,
-    tripsCreated: { currentMonth: 150, lastMonth: 250 },
-    userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
-  };
-
-  const { totalTrips, totalUsers, tripsCreated, userRole, usersJoined } =
-    dashboardStats;
-
   return (
     <main className="dashboard wrapper">
       <Header
@@ -23,27 +13,45 @@ const Dashboard = () => {
       />
 
       <section className="flex flex-col gap-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users Today"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
       </section>
-      <StatsCard
-        headerTitle="Total Users"
-        total={totalUsers}
-        currentMonthCount={usersJoined.currentMonth}
-        lastMonthCount={usersJoined.lastMonth}
-      />
-      <StatsCard
-        headerTitle="Total Trips"
-        total={totalTrips}
-        currentMonthCount={tripsCreated.currentMonth}
-        lastMonthCount={tripsCreated.lastMonth}
-      />
-      <StatsCard
-        headerTitle="Active Users Today"
-        total={userRole.total}
-        currentMonthCount={userRole.currentMonth}
-        lastMonthCount={userRole.lastMonth}
-      />
-      <TripCard />
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips
+            .slice(0, 4)
+            .map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+              <TripCard
+                key={id}
+                id={id.toString()}
+                name={name}
+                imageUrls={imageUrls[0]}
+                location={itinerary?.[0]?.location ?? ""}
+                tags={tags}
+                price={estimatedPrice}
+              />
+            ))}
+        </div>
+      </section>
     </main>
   );
 };
--- components/trip-card.tsx
+++ components/trip-card.tsx
@@ -1,5 +1,57 @@
-const TripCard = () => {
-  return <div>TripCard</div>;
+import { Link, useLocation } from "react-router";
+import {
+  ChipListComponent,
+  ChipsDirective,
+  ChipDirective,
+} from "@syncfusion/ej2-react-buttons";
+import { cn, getFirstWord } from "~/lib/utils";
+
+const TripCard = ({ id, name, location, imageUrls, tags, price }: any) => {
+  const path = useLocation();
+
+  return (
+    <>
+      <Link
+        to={
+          path.pathname === "/" || path.pathname.startsWith("/travel")
+            ? `/travel/${id}`
+            : `/trips/${id}`
+        }
+        className="trip-card"
+      >
+        <img src={imageUrls} alt={name} />
+        <article>
+          <h2>{name}</h2>
+          <figure>
+            <img
+              src="/assets/icons/location-mark.svg"
+              alt="location"
+              className="size-4"
+            />
+            <figcaption>{location}</figcaption>
+          </figure>
+        </article>
+        <div className="mt-5 pl-4.5 pr-3.5 pb-5">
+          <ChipListComponent>
+            <ChipsDirective>
+              {tags.map((tag, index) => (
+                <ChipDirective
+                  key={index}
+                  text={getFirstWord(tag)}
+                  cssClass={cn(
+                    index === 1
+                      ? "!bg-pink-50 !text-pink-500"
+                      : "!bg-success-50 !text-success-700",
+                  )}
+                />
+              ))}
+            </ChipsDirective>
+          </ChipListComponent>
+        </div>
+        <article className="tripCard-pill">{price}</article>
+      </Link>
+    </>
+  );
 };
 
 export default TripCard;
